d62294101586865ff941acaa3e31b4e6ac0145cd
Nirupam Shuvo <39645954+nirupamshuvo@users.noreply.github.com>
2021-08-23 20:10:08 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+/*   <!--Using Ashtric symbol for commenting the whole javascript code -->
+
+    alert("Nirupam Shuvo");//----alert fuction//
+    document.write("<h1>I love Bangladesh</h1>");//-------Normal javascript print// 
+    console.log("I love Finland");//--- Can go to the browser's console then able to see output
+
+    //----- Normal print value //
+     var name="Nirupam Sutradhar Shuvo </br>";
+
+        document.write(name);
+
+        
+        var firstName,lastName,age;
+        
+        firstName="Nirupam </br> ";
+        lastName="Anisul Islam </br>";
+        age="25";
+
+        document.write(firstName);
+        document.write(lastName);
+        document.write(age);
+
+
+        var num = 20.56789;
+
+        num = toString(num);  //------Number to string//
+
+        num = parseInt(num); //--------integer//
+
+        console.log(typeof(num)); //-------which type int or string or float// 
+
+        console.log(num.toFixed(3)); //-------after floating point fixed value//
+
+        console.log(typeof(Number(12))); //-----identify type is it int or string or float// 
+
+var firstName = "Anisul Islam";
+
+document.write(firstName+" is my name");
+
+var num1 = 30;
+
+var num2 = 20;
+
+document.write("Num1 = "+ num1 + " and Num2 = "+num2 );
+//-----concate the numbers//
+document.write(text1.concat(text2));
+
+document.write(text.charAt(0));
+text = text.toUpperCase();
+document.write(text);
+
+//-----text length identify//
+
+var text = prompt("Enter your name : ");
+var len = text.length;
+document.write("Number of character "+text.length);
+
+var text1 = "Bangladesh";
+var text2 = " is a Beautiful Country";
+var text = text1.concat(text2);
+
+document.write(text);
+
+//----Basic calculator operator// 
+var num1 = prompt("Enter the 1st number : ");
+var num2 = prompt("Enter the 2nd number : ");
+
+num1 = parseInt(num1, 10);
+num2 = parseInt(num2, 10);
+
+var sum,sub;
+
+sum = num1 + num2;
+
+sum = document.write(num1+" + "+num2+" = "+sum+"</br>");
+
+sub =  num1 - num2;
+
+sub = document.write(num1+" - "+num2+" = "+sub+"</br>");
+
+*/
+
+
+
